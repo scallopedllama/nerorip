87387a20a969c05df6c40e3f8986502cd735e162
Joe Balough <scallopedllama@gmail.com>
2011-05-13 15:21:56 +0900

    Change the read functions to swap endianness before returning, which fixed the chunk offset issue I was having before.

--- main.c
+++ main.c
@@ -3,33 +3,46 @@
 #include <errno.h>
 #include <string.h> // strerror()
 #include <stdint.h> // uintXX_t types
+#include <byteswap.h> // for bswap_XX functions
+
+#define NER5 0x4e455235
+#define NERO 0x4e45524f
+#define version1 1
+#define version2 2
 
 
-uint16_t fread16(FILE* input) {
+/**
+ * File reading convenience functions
+ *
+ * These functions read an unsigned integer of the indicated size from the
+ * passed file and handles read errors and the like.
+ * Before returning, convert the number from little endian to big endian.
+ */
+uint16_t fread16u(FILE* input) {
   uint16_t r;
   if (fread(&r, sizeof(uint16_t), 1, input) != 1) {
     fprintf(stderr, "Error reading from file: %s\n", strerror(errno));
     exit(EXIT_FAILURE);
   }
-  return r;
+  return bswap_16(r);
 }
 
-uint32_t fread32(FILE* input) {
+uint32_t fread32u(FILE* input) {
   uint32_t r;
   if (fread(&r, sizeof(uint32_t), 1, input) != 1) {
     fprintf(stderr, "Error reading from file: %s\n", strerror(errno));
     exit(EXIT_FAILURE);
   }
-  return r;
+  return bswap_32(r);
 }
 
-uint64_t fread64(FILE* input) {
+uint64_t fread64u(FILE* input) {
   uint64_t r;
   if (fread(&r, sizeof(uint64_t), 1, input) != 1) {
     fprintf(stderr, "Error reading from file: %s\n", strerror(errno));
     exit(EXIT_FAILURE);
   }
-  return r;
+  return bswap_64(r);
 }
 
 
@@ -42,18 +55,26 @@ int main(int argc, char **argv) {
     exit(EXIT_FAILURE);
   }
 
-  // The header is actually stored in the last 8 or 12 bytes of the file, so seek to those positions and get reading
+  int image_ver;
+  // The header is actually stored in the last 8 or 12 bytes of the file, depending on the version.
+  // Seek to 12 bytes from the end and see if it's version 2. If so, get the header.
+  uint64_t first_chunk_offset;
   fseek(input_image, -12, SEEK_END);
-  uint32_t v2_head_txt = fread32(input_image);
-  uint64_t v2_file_offset = fread64(input_image);
 
+  if (fread32u(input_image) == NER5) {
+    first_chunk_offset = fread64u(input_image);
+    image_ver = version2;
+  }
+  else if (fread32u(input_image) == NERO) {
+    first_chunk_offset = (uint64_t) fread32u(input_image);
+    image_ver = version1;
+  }
+  else {
+    fprintf(stderr, "This does not appear to be a Nero image. Aborting.\n");
+    exit(EXIT_FAILURE);
+  }
 
-  fseek(input_image, -8, SEEK_END);
-  uint32_t v1_head_txt = fread32(input_image);
-  uint32_t v1_file_offset = fread32(input_image);
-
-  printf("If version 1: Head text: %x, File offset %x\n", v1_head_txt, v1_file_offset);
-  printf("If version 2: Head text: %x, File offset %x\n", v2_head_txt, v2_file_offset);
+  printf("Image is a version %d Nero image with first chunk offset of %X\n", image_ver, first_chunk_offset);
 
 
   fclose(input_image);
