f5591f4429c207bd4987a8a9fad973568a04786f
Joe Balough <scallopedllama@gmail.com>
2011-05-16 19:50:01 +0900

    updated the help a little bit. Need to change the readme too.

--- main.c
+++ main.c
@@ -29,22 +29,26 @@
 
 void usage(char *argv0) {
   fprintf(stderr, "Usage: %s [OPTIONS]... [INPUT FILE] [OUTPUT DIRECTORY]\n", argv0);
-  fprintf(stderr, "Nero Image Ripper takes a nero image file (.nrt extension) as input\n");
+  fprintf(stderr, "Nerorip takes a nero image file (.nrt extension) as input\n");
   fprintf(stderr, "and attempts to extract the track data as either ISO or audio data.\n\n");
   fprintf(stderr, "  -i, --info\t\tOnly disply information about the image file, do not rip\n");
+  fprintf(stderr, "  -v, --verbose\t\tIncrement program verbosity by one tick\n");
+  fprintf(stderr, "  -q, --quiet\t\tDecrement program verbosity by one tick\n");
+  fprintf(stderr, "             \t\tVerbosity starts at 1, a verbosity of 0 will print nothing.\n");
   fprintf(stderr, "  -h, --help\t\tDisplay this help message and exit\n");
-  fprintf(stderr, "  -v, --version\t\tOutput version information and exit.\n\n");
+  fprintf(stderr, "      --version\t\tOutput version information and exit.\n\n");
   fprintf(stderr, "If output directory is omitted, image data is put in the same directory as the input file.\n\n");
 
-  fprintf(stderr, "For each track found in the image, Nero Image Ripper will output the following:\n");
+  fprintf(stderr, "For each track found in the image, nerorip will output the following:\n");
   fprintf(stderr, "  one iso file named \"data.sSStTT.iso\" if the track is data and\n");
   fprintf(stderr, "  one wav file named \"audio.sSStTT.wav\" if the track is audio\n");
-  fprintf(stderr, "where SS is the session number and TT is the track number.\n\n");
+  fprintf(stderr, "where SS is the session number and TT is the track number.\n");
+  fprintf(stderr, "Also, for each session in the iamge, nerorip will output one cue file.");
 
   fprintf(stderr, "For example, if your disc image is like the following\n");
   fprintf(stderr, "  Session 1:\n    Track 1: Audio\n    Track 2: Data\n  Session 2:\n    Track 1: Data\n");
-  fprintf(stderr, "the Nero Image Ripper will output the following files:\n");
-  fprintf(stderr, "  audio.s01t01.wav, data.s01t02.iso, data.s02t03.iso\n");
+  fprintf(stderr, "nerorip will output the following files:\n");
+  fprintf(stderr, "  audio.s01t01.wav, data.s01t02.iso, data.s02t03.iso, session01.cue, session02.cue\n");
   fprintf(stderr, "Note that the track number does not reset between sessions.\n\n");
 
   fprintf(stderr, "Report all bugs at https://github.com/scallopedllama/nerorip\nVersion %s\n", VERSION);
